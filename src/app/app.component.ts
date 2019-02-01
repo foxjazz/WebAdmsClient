201e7c40400fff7cdfc4c0e2a7f942ef85caab04
mordagel <mordagel@outlook.com>
2019-01-31 19:34:17 -0700

    first draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { EveSystem, Adm } from './models/model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'WebAdmClient';
+  public eveSystems: EveSystem[];
+
+  public adm: Adm;
+  ngOnInit() {
+    this.populateEsys();
+    this.adm = {name: "", id: 0};
+  }
+  public save() {
+
+  }
+  public remove (a: string, iid: number){
+    const data = this.eveSystems.filter(a => a.id === iid);
+    for(let i = 0; i < data[0].adms.length; i++){
+      if(data[0].adms[i].name === a) {
+        data[0].adms.splice(i, 1);
+      }
+    }
+  }
+  public addTag(e: string, iid: number) {
+    const data = this.eveSystems.filter(a => a.id === iid);
+    data[0].adms.push({name: e, id: 1});
+  }
+  private populateEsys(): any {
+    this.eveSystems = [];
+    const ee = this.eveSystems;
+    console.log("starting");
+
+    ee.push({name: "5XR-KZ", id: 1, adms: []});
+    ee.push({name: "75C-WN", id: 2, adms: []});
+    ee.push({name: "BG-W90", id: 3, adms: []});
+    ee.push({name: "C-0ND2", id: 4, adms: []});
+    ee.push({name: "I5Q2-S", id: 5, adms: []});
+    ee.push({name: "JI-LGM", id: 6, adms: []});
+    ee.push({name: "OCU4-R", id: 7, adms: []});
+    ee.push({name: "PO-3QW", id: 8, adms: []});
+    ee.push({name: "VF-FN6", id: 9, adms: []});
+    ee.push({name: "Z-PNIA", id: 10, adms: []});
+  }
+  addAdm(id: number, adm: Adm) {
+    const sys = this.eveSystems.filter(a => a.id === id);
+    sys[0].adms.push(adm);
+  }
 }
---
+++ src/app/models/model.ts
@@ -0,0 +1,11 @@
+export interface EveSystem {
+    id: number;
+    name: string;
+    adms: Adm[];
+}
+
+export interface Adm {
+    id: number;
+    name: string;
+}
+
